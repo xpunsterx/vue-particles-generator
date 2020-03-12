14c0fd506fd938e8b1d2ac71d1a37478626d0789
xpunsterx <punsterde@gmail.com>
2020-03-13 00:29:12 +0300

    added docs

---
+++ docs/precache-manifest.d956abed27ee903e11d969c9bf4ab76f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3044cfdb35e7b562d441",
+    "url": "/vue-particles-generator/css/about.f6c37b11.css"
+  },
+  {
+    "revision": "e9c16496d692893bf6ab",
+    "url": "/vue-particles-generator/css/app.a4936770.css"
+  },
+  {
+    "revision": "504fa8bcee8ef584f202",
+    "url": "/vue-particles-generator/css/chunk-vendors.aacc95eb.css"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/vue-particles-generator/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/vue-particles-generator/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "8484bdeab0f39d39881be5004502313a",
+    "url": "/vue-particles-generator/index.html"
+  },
+  {
+    "revision": "3044cfdb35e7b562d441",
+    "url": "/vue-particles-generator/js/about.26612fad.js"
+  },
+  {
+    "revision": "e9c16496d692893bf6ab",
+    "url": "/vue-particles-generator/js/app.3a2229df.js"
+  },
+  {
+    "revision": "504fa8bcee8ef584f202",
+    "url": "/vue-particles-generator/js/chunk-vendors.8afc72cd.js"
+  },
+  {
+    "revision": "1c3ae08b17c21c72153d93c39168acc9",
+    "url": "/vue-particles-generator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-particles-generator/robots.txt"
+  }
+]);
